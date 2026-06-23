a95d6f9d71e19b91d04cf5b42a0f2c59f733637c
floryes768-svg <floryes768@gmail.com>
2026-06-23 00:51:30 -0500

    agregar postgresql y dj-database-url

--- siae/settings.py
+++ siae/settings.py
@@ -54,11 +54,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'siae.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
