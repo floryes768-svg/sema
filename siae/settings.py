a015db347797181fb06ea72b0aefcb856c0da846
floryes768-svg <floryes768@gmail.com>
2026-06-23 00:23:55 -0500

    simplificar static files

--- siae/settings.py
+++ siae/settings.py
@@ -75,6 +75,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
